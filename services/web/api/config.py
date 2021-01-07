a2f8e6fc23de6bfacdb423ac3663ed10ce3d8eb2
Aswin Krishnamoorthy <Aswin.Krishnamoorthy@hogarthww.com>
2021-01-07 16:04:26 +0530

    'config_changes'

--- services/web/api/config.py
+++ services/web/api/config.py
@@ -9,10 +9,10 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/api/static/"
     MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/api/media/"
-    SECRET_KEY = '34a19f17d155f928c31bd1f6de8de16f2ca1d3d7605f3278'
-    MAIL_SERVER = 'smtp.mailtrap.io'
-    MAIL_PORT = 2525
-    MAIL_USERNAME = '582c1169b62dba'
-    MAIL_PASSWORD = '954c6d89f7685c'
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL")
