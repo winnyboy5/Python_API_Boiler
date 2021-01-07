48c316d876b5331d69b58cf2e5dce123adfa95db
Aswin Krishnamoorthy <Aswin.Krishnamoorthy@hogarthww.com>
2021-01-07 21:28:07 +0530

    'config_changes'

--- services/web/mods/users/models/user_model.py
+++ services/web/mods/users/models/user_model.py
@@ -1,6 +1,6 @@
 from api.extensions import db, ma
 from utils.db_utils import TimestampMixin
-from flask_bcrypt import generate_password_hash, check_password_hash
+# from flask_bcrypt import generate_password_hash, check_password_hash
 
 
 class UserModel(db.Model, TimestampMixin):
@@ -11,13 +11,11 @@ class UserModel(db.Model, TimestampMixin):
     active = db.Column(db.Boolean(), default=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
-    def __init__(self, email, phone, password):
-        self.email = email
-        self.phone = phone
-        self.password = generate_password_hash(password).decode('utf8')
+    def __init__(self, **kwargs):
+        super(UserModel, self).__init__(**kwargs)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    # def check_password(self, password):
+    #     return check_password_hash(self.password, password)
 
 
 class UserSchema(ma.Schema):
